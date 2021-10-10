ee5b71ebc57f01092d948a977b064da27cdfde0c
Your Name <you@example.com>
2021-10-10 16:46:26 -0600

    switch to neural net predict

--- train_ml_model.py
+++ train_ml_model.py
@@ -1,9 +1,15 @@
+from typing import List
 import pandas as pd
 import numpy as np
 import glob
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import r2_score
 import xgboost as xgb
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 
 
 def main():
@@ -18,6 +24,14 @@ def main():
 
 
 def _load_csvs(player_data=pd.DataFrame()) -> pd.DataFrame:
+    """Loads in CSV stats downloaded from https://www.naturalstattrick.com/playerteams.php
+
+    Args:
+        player_data ([type], optional): stupid way to initialize a frame. Defaults to pd.DataFrame().
+
+    Returns:
+        pd.DataFrame: DataFrame of all the data
+    """
     path = r"csvs"
     all_files = glob.glob(path + "/*.csv")
     for file_name in all_files:
@@ -38,6 +52,7 @@ def _load_csvs(player_data=pd.DataFrame()) -> pd.DataFrame:
                         "Total Points",
                         "Shots",
                         "GP",
+                        "Position",
                     ]
                 ].rename(
                     columns={
@@ -71,66 +86,52 @@ def _load_csvs(player_data=pd.DataFrame()) -> pd.DataFrame:
                         "GP": f"gp_{int(file_name[5:-4])}",
                     }
                 )
-                player_data = player_data.merge(_player_data, on="Player", how="outer")
-    player_data = player_data
-    return player_data.merge(age_data, how="left", on="Player")
+                player_data = player_data.merge(_player_data, on=["Player", "Position"], how="outer")
+    player_data = player_data.groupby("Player", group_keys=False).apply(_fix_multiple_positions)
+    return _scale_player_data(player_data)
+
+
+def _fix_multiple_positions(player_data: pd.DataFrame) -> pd.DataFrame:
+    if len(player_data) == 1:
+        return player_data
+    player = player_data["Player"].iloc[0]
+    return player_data.agg(sum).to_frame().T.assign(Player=player)
+
+
+def _scale_player_data(player_data: pd.DataFrame) -> pd.DataFrame:
+    float_columns = player_data.drop(columns=["Player", "Position"])
+    string_columns = player_data[["Player", "Position"]]
+    scaler = MinMaxScaler()
+    scaled_player_data = scaler.fit_transform(float_columns)
+    return pd.DataFrame(np.hstack([string_columns, scaled_player_data]), columns=list(string_columns.columns) + list(float_columns.columns))
 
 
 def _transform_player_data(player_data: pd.DataFrame) -> pd.DataFrame:
-    target_list = ["goals_20", "assists_20", "PIM_20", "shots_20"]
-    features_list = [
-        "Player",
-        "Position",
-        "Age",
-        "goals_17",
-        "goals_18",
-        "goals_19",
-        "assists_17",
-        "assists_18",
-        "assists_19",
-        "points_17",
-        "points_18",
-        "points_19",
-        "shots_17",
-        "shots_18",
-        "shots_19",
-        "gp_17",
-        "gp_18",
-        "gp_19",
-        "PIM_17",
-        "PIM_18",
-        "PIM_19",
-    ]
+    features = []
+    targets = []
+    feature_columns = ["Player", "Position", "goals_3", "assists_3", "PIM_3", "shots_3", "goals_2", "assists_2", "PIM_2", "shots_2", "goals_1", "assists_1", "PIM_1", "shots_1"]
+    target_columns = ["goals_0", "assists_0", "PIM_0", "shots_0"]
+    for year in [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]:
+        features.append(pd.DataFrame(player_data[_create_feature_list(year)].values, columns=feature_columns))
+        targets.append(pd.DataFrame(player_data[_create_target_list(year)].values, columns=target_columns))
+    full_features = pd.DataFrame(player_data[_create_feature_list(22)].fillna(0).values, columns=feature_columns)
+    features = pd.concat(features, ignore_index=True)
+    targets = pd.concat(targets, ignore_index=True)
+    both = features.join(targets).fillna(0)
+    features = both[feature_columns]
+    targets = both[target_columns]
+    return targets, features, full_features
 
-    full_features_list = [
-        "Player",
-        "Position",
-        "Age",
-        "goals_18",
-        "goals_19",
-        "goals_20",
-        "assists_18",
-        "assists_19",
-        "assists_20",
-        "points_18",
-        "points_19",
-        "points_20",
-        "shots_18",
-        "shots_19",
-        "shots_20",
-        "gp_18",
-        "gp_19",
-        "gp_20",
-        "PIM_18",
-        "PIM_19",
-        "PIM_20",
-    ]
 
-    cleaned_df = player_data[features_list + target_list].dropna()
-    target = cleaned_df[target_list]
-    features = cleaned_df[features_list]
-    full_features = player_data[full_features_list].dropna()
-    return target, features, full_features
+def _create_target_list(year: int) -> List[str]:
+    return [f"goals_{year}", f"assists_{year}", f"PIM_{year}", f"shots_{year}"]
+
+
+def _create_feature_list(target_year: int) -> List[str]:
+    feature_list = ["Player", "Position"]
+    for year in range(target_year - 3, target_year):
+        feature_list.extend([f"goals_{year}", f"assists_{year}", f"PIM_{year}", f"shots_{year}"])
+    return feature_list
 
 
 def _predict(target: pd.DataFrame, features: pd.DataFrame, full_features: pd.DataFrame):
@@ -138,27 +139,38 @@ def _predict(target: pd.DataFrame, features: pd.DataFrame, full_features: pd.Dat
         features.drop(columns=["Player", "Position"]), target, test_size=0.25, random_state=42
     )
 
-    parameters, best_features = _gridsearch_xgb(train_features, train_target)
-    model = xgb.XGBRegressor()
-    # parameters = {'base_score': None, 'booster': 'dart', 'colsample_bylevel': 0.8, 'colsample_bynode': 0.5, 'colsample_bytree': 0.8, 'gamma': 1, 'learning_rate': 0.1, 'max_depth': None, 'min_child_weight': 0, 'n_estimators': 1000, 'num_parallel_tree': 5, 'objective': 'reg:squarederror', 'reg_alpha': 1.5, 'reg_lambda': 1.5, 'subsample': 0.8, 'tree_method': 'exact'}
-    model.set_params(**parameters)
-    model.fit(np.array(train_features), np.array(train_target).ravel())
-    target_pred = model.predict(np.array(test_features))
-    final_pred = model.predict(np.array(full_features))
+    model = _build_neural_net(in_shape=train_features.shape[1], out_shape=1)
+    model.fit(train_features.values, train_target.values, epochs=15, verbose=0)
+    target_pred = model.predict(test_features.values)
+    final_pred = model.predict(full_features.values)
 
     return r2_score(test_target, target_pred), final_pred
 
 
+def _build_neural_net(in_shape: int, out_shape: int):
+    """
+    Creates the neural net model
+    """
+    model = Sequential()
+    model.add(Dense(units=150, activation="relu", input_dim=in_shape))
+    for _ in range(15):
+        model.add(Dense(units=150, activation="relu"))
+    model.add(Dense(units=out_shape, activation="linear"))
+
+    model.compile(loss="mse", optimizer=Adam(lr=0.00001))
+    return model
+
 
 def _calc_new_season(targets: pd.DataFrame, features: pd.DataFrame, full_features: pd.DataFrame, output: pd.DataFrame) -> pd.DataFrame:
-
     for target in targets:
         target_frame = targets[target]
         r2, target_pred = _predict(target_frame, features, full_features.drop(columns=["Player", "Position"]))
-        print(f"{target[:-3]} R2: {r2:.2f}")
-        output[f"{target[:-3]}"] = target_pred
-
+        print(f"{target[:-2]} R2: {r2:.2f}")
+        output[f"{target[:-2]}"] = target_pred
     return output
+    # r2, target_pred = _predict(targets, features, full_features.drop(columns=["Player", "Position"]))
+    # print(r2)
+    # return output.join(pd.DataFrame(target_pred, columns=targets.columns.str[:-3]))
 
 
 def _rank_players(prediction_df: pd.DataFrame) -> pd.DataFrame:
@@ -167,24 +179,17 @@ def _rank_players(prediction_df: pd.DataFrame) -> pd.DataFrame:
     right = prediction_df[prediction_df["Position"].str.contains("R")].reset_index()
     defence = prediction_df[prediction_df["Position"].str.contains("D")].reset_index()
 
-    for target in ["goals", "assists", "PIM", "shots"]:
-        centres[f"{target}"] = centres[f"{target}"].rank(ascending=False)
-        left[f"{target}"] = left[f"{target}"].rank(ascending=False)
-        right[f"{target}"] = right[f"{target}"].rank(ascending=False)
-        defence[f"{target}"] = defence[f"{target}"].rank(ascending=False)
-        prediction_df[f"{target}"] = prediction_df[f"{target}"].rank(ascending=False)
-
-    centres = centres.assign(rank=lambda x: x["goals"] + x["assists"] + x["PIM"] + x["shots"]).sort_values("rank", ascending=True)
-    left = left.assign(rank=lambda x: x["goals"] + x["assists"] + x["PIM"] + x["shots"]).sort_values("rank", ascending=True)
-    right = right.assign(rank=lambda x: x["goals"] + x["assists"] + x["PIM"] + x["shots"]).sort_values("rank", ascending=True)
-    defence = defence.assign(rank=lambda x: x["goals"] + x["assists"] + x["PIM"] + x["shots"]).sort_values("rank", ascending=True)
-    prediction_df = prediction_df.assign(rank=lambda x: x["goals"] + x["assists"] + x["PIM"] + x["shots"]).sort_values("rank", ascending=True)
+    centres = centres.assign(rank=lambda x: x["goals"] + x["assists"] + x["PIM"] + x["shots"]).sort_values("rank", ascending=False)
+    left = left.assign(rank=lambda x: x["goals"] + x["assists"] + x["PIM"] + x["shots"]).sort_values("rank", ascending=False)
+    right = right.assign(rank=lambda x: x["goals"] + x["assists"] + x["PIM"] + x["shots"]).sort_values("rank", ascending=False)
+    defence = defence.assign(rank=lambda x: x["goals"] + x["assists"] + x["PIM"] + x["shots"]).sort_values("rank", ascending=False)
+    prediction_df = prediction_df.assign(rank=lambda x: x["goals"] + x["assists"] + x["PIM"] + x["shots"]).sort_values("rank", ascending=False)
 
     print(centres.head(20))
     print(left.head(20))
     print(right.head(20))
     print(defence.head(20))
-    print(prediction_df.head(80))
+    print(prediction_df.head(20))
     import pdb; pdb.set_trace()
 
 
